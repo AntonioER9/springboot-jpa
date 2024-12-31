340c90039c3585ef7d3982c55bcc32a7bc577536
Antonio Espinoza (Chile) <a0e0bhj@m-p3j23wmljw.homeoffice.wal-mart.com>
2024-12-31 10:48:43 -0300

    Implement PersonRepository and update SpringbootJpaApplication to fetch and display all persons

--- src/main/java/com/andres/course/springboot/jpa/springboot_jpa/SpringbootJpaApplication.java
+++ src/main/java/com/andres/course/springboot/jpa/springboot_jpa/SpringbootJpaApplication.java
@@ -1,11 +1,20 @@
 package com.andres.course.springboot.jpa.springboot_jpa;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.andres.course.springboot.jpa.springboot_jpa.entities.Person;
+import com.andres.course.springboot.jpa.springboot_jpa.repositories.PersonRepository;
+
 @SpringBootApplication
 public class SpringbootJpaApplication implements CommandLineRunner {
+
+	@Autowired
+	private PersonRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootJpaApplication.class, args);
@@ -13,7 +22,8 @@ public class SpringbootJpaApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-
+		List<Person> persons = (List<Person>) repository.findAll(); // findAll() returns an Iterable, so we cast it to List
+		persons.stream().forEach(person -> System.out.println(person));
 	}
 
 }
