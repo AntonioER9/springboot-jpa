e616988df7b4bd93780de11760f42023ffc7a2f7
Antonio Espinoza (Chile) <a0e0bhj@m-p3j23wmljw.homeoffice.wal-mart.com>
2025-01-03 12:11:14 -0300

    Add findOne and findOneName methods to PersonRepository and implement findOne in SpringbootJpaApplication

--- src/main/java/com/andres/course/springboot/jpa/springboot_jpa/SpringbootJpaApplication.java
+++ src/main/java/com/andres/course/springboot/jpa/springboot_jpa/SpringbootJpaApplication.java
@@ -1,6 +1,7 @@
 package com.andres.course.springboot.jpa.springboot_jpa;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -26,6 +27,19 @@ public class SpringbootJpaApplication implements CommandLineRunner {
 		// returns an Iterable, so we cast it to List
 		List<Person> persons = (List<Person>) repository.searchByProgrammingLanguage("Java", "Andres");
 		persons.stream().forEach(person -> System.out.println(person));
+	}
+
+	public void findOne() {
+
+		Person person = null;
+		Optional<Person> optionalPerson = repository.findById(1L);
+		if (optionalPerson.isPresent()) {
+			person = optionalPerson.get();
+		}
+		System.out.println(person);
+		// repository.findOneName("Andres").ifPresent(person ->
+		// System.out.println(person));
+		repository.findOneName("Andres").ifPresent(System.out::println);
 	}
 
 }
